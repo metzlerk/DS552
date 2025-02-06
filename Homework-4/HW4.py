8fc33c82635c31958af9040a26860753df739772
Kevin Metzler <kjmetzler@wpi.edu>
2025-02-06 16:03:52 -0500

    Ran ICFAR to get images, they don't look great

---
+++ Homework-4/HW4.py
@@ -0,0 +1,119 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+import os
+import matplotlib.pyplot as plt
+
+# Disable GPU usage (commented out to enable GPU)
+tf.config.set_visible_devices([], 'GPU')
+print("Using GPU if available.")
+
+# Directory to save generated images
+if not os.path.exists('generated_images'):
+    os.makedirs('generated_images')
+
+def save_images(epoch, test_input):
+    predictions = generator(test_input, training=False)
+    fig = plt.figure(figsize=(4, 4))
+
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i+1)
+        plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
+        plt.axis('off')
+
+    plt.savefig('generated_images/image_at_epoch_{:04d}.png'.format(epoch))
+    plt.close()
+
+# Load and preprocess data
+(train_images, _), (_, _) = tf.keras.datasets.mnist.load_data()
+train_images = train_images.reshape(-1, 28, 28, 1).astype('float32')
+train_images = (train_images - 127.5) / 127.5 # Normalize to [-1, 1]
+
+BUFFER_SIZE = 60000
+BATCH_SIZE = 256
+
+train_dataset = tf.data.Dataset.from_tensor_slices(train_images).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
+
+# Generator model
+def make_generator_model():
+    model = tf.keras.Sequential([
+        layers.Dense(7*7*256, use_bias=False, input_shape=(100,)),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(),
+        layers.Reshape((7, 7, 256)),
+        layers.Conv2DTranspose(256, (5, 5), strides=(1, 1), padding='same', use_bias=False),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(),
+        layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(),
+        layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(),
+        layers.Conv2DTranspose(32, (5, 5), strides=(1, 1), padding='same', use_bias=False),
+        layers.BatchNormalization(),
+        layers.LeakyReLU(),
+        layers.Conv2DTranspose(1, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation='tanh')
+    ])
+    return model
+
+# Discriminator model
+def make_discriminator_model():
+    model = tf.keras.Sequential([
+        layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=[28, 28, 1]),
+        layers.LeakyReLU(),
+        layers.Dropout(0.3),
+        layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'),
+        layers.LeakyReLU(),
+        layers.Dropout(0.3),
+        layers.Flatten(),
+        layers.Dense(1)
+    ])
+    return model
+
+# Loss functions
+cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+def discriminator_loss(real_output, fake_output):
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    return real_loss + fake_loss
+
+def generator_loss(fake_output):
+    return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+# Optimizers
+generator = make_generator_model()
+discriminator = make_discriminator_model()
+generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+
+# Training function
+@tf.function
+def train_step(images):
+    noise = tf.random.normal([BATCH_SIZE, 100])
+
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+        generated_images = generator(noise, training=True)
+        real_output = discriminator(images, training=True)
+        fake_output = discriminator(generated_images, training=True)
+        gen_loss = generator_loss(fake_output)
+        disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
+    generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+    discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+# Training loop
+def train(dataset, epochs):
+    seed = tf.random.normal([16, 100])
+    for epoch in range(epochs):
+        for image_batch in dataset:
+            train_step(image_batch)
+        
+        # Save the model every 10 epochs
+        if (epoch + 1) % 10 == 0:
+            save_images(epoch + 1, seed)
+
+# Run the training
+train(train_dataset, epochs=500)
