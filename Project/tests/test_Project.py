4e9b813ba02b7d4810fa2eadbcc573c9d9ec7e08
Kevin Metzler <kjmetzler@wpi.edu>
2025-03-31 14:12:16 -0400

    Add initial project structure with .gitignore, requirements, and test cases

---
+++ Project/tests/test_Project.py
@@ -0,0 +1,11 @@
+def test_backend_api():
+    assert True
+
+def test_encounter_generation():
+    assert True
+
+def test_dungeon_creation():
+    assert True
+
+def test_npc_creation():
+    assert True
